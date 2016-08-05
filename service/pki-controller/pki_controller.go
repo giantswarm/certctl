8af7ed56518a7e06c5495b8cbd2c351d6d04effd
Tim Schindler <xh3b4sd@gmail.com>
2016-08-05 11:36:15 +0200

    Pki (#13)
    * first draft of PKI controller

---
+++ service/pki-controller/pki_controller.go
@@ -0,0 +1,118 @@
+package pkicontroller
+
+import (
+	"fmt"
+	"net/http"
+
+	vaultclient "github.com/hashicorp/vault/api"
+
+	"github.com/giantswarm/certctl/service/spec"
+)
+
+// Config represents the configuration used to create a new PKI controller.
+type Config struct {
+	// Dependencies.
+	VaultClient *vaultclient.Client
+}
+
+// DefaultConfig provides a default configuration to create a PKI controller.
+func DefaultConfig() Config {
+	newClientConfig := vaultclient.DefaultConfig()
+	newClientConfig.Address = "http://127.0.0.1:8200"
+	newClientConfig.HttpClient = http.DefaultClient
+	newVaultClient, err := vaultclient.NewClient(newClientConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	newConfig := Config{
+		// Dependencies.
+		VaultClient: newVaultClient,
+	}
+
+	return newConfig
+}
+
+// New creates a new configured PKI controller.
+func New(config Config) (spec.PKIController, error) {
+	newPKIController := &pkiController{
+		Config: config,
+	}
+
+	// Dependencies.
+	if newPKIController.VaultClient == nil {
+		return nil, maskAnyf(invalidConfigError, "Vault client must not be empty")
+	}
+
+	return newPKIController, nil
+}
+
+type pkiController struct {
+	Config
+}
+
+// PKI management.
+
+func (pc *pkiController) SetupPKIBackend(config spec.PKIConfig) error {
+	// Create a client for the system backend configured with the Vault token
+	// used for the current cluster's PKI backend.
+	sysBackend := pc.VaultClient.Sys()
+	// Mount a new PKI backend for the cluster, if it does not already exist.
+	mounts, err := sysBackend.ListMounts()
+	if err != nil {
+		return maskAny(err)
+	}
+	config, ok := mounts[pc.MountPath(config.ClusterID)+"/"]
+	if !ok || config.Type != "pki" {
+		newMountConfig := &vaultclient.MountInput{
+			Type:        "pki",
+			Description: fmt.Sprintf("PKI backend for cluster ID '%s'", config.ClusterID),
+			Config: vaultclient.MountConfigInput{
+				MaxLeaseTTL: config.TTL,
+			},
+		}
+		err = sysBackend.Mount(pc.MountPath(config.ClusterID), newMountConfig)
+		if err != nil {
+			return maskAny(err)
+		}
+	}
+
+	// Create a client for the logical backend configured with the Vault token
+	// used for the current cluster's root CA and role.
+	logicalStore := pc.VaultClient.Logical()
+	// Generate a certificate authority for the PKI backend.
+	data := map[string]interface{}{
+		"ttl":         config.TTL,
+		"common_name": config.CommonName,
+	}
+	_, err := logicalStore.Write(pc.CAPath(config.ClusterID), data)
+	if err != nil {
+		return maskAny(err)
+	}
+	// Create role for the mounted PKI backend.
+	data = map[string]interface{}{
+		"allowed_domains":  config.AllowedDomains,
+		"allow_subdomains": "true",
+		"ttl":              config.TTL,
+	}
+	_, err = logicalStore.Write(pc.RolePath(config.ClusterID), data)
+	if err != nil {
+		return maskAny(err)
+	}
+
+	return nil
+}
+
+// Path management.
+
+func (pc *pkiController) CAPath(clusterID string) string {
+	return fmt.Sprintf("pki-%s/root/generate/exported", clusterID)
+}
+
+func (pc *pkiController) MountPath(clusterID string) string {
+	return fmt.Sprintf("pki-%s", clusterID)
+}
+
+func (pc *pkiController) RolePath(clusterID string) string {
+	return fmt.Sprintf("pki-%s/roles/role-%s", clusterID, clusterID)
+}
---
+++ service/spec/pki_controller.go
@@ -0,0 +1,58 @@
+package spec
+
+// PKIConfig is used to configure the setup of a PKI backend done by the
+// PKIController.
+type PKIConfig struct {
+	// AllowedDomains represents a comma separate list of valid domain names the
+	// generated certificate authority is valid for.
+	AllowedDomains string `json:"allowed_domains"`
+
+	// ClusterID represents the cluster ID a PKI backend setup should be done
+	// for. This ID is used to restrict access on Vault related operations for a
+	// specific cluster. E.g. the Vault PKI backend will be mounted on a path
+	// containing this ID. That way Vault policies will restrict access to this
+	// specific path.
+	ClusterID string `json:"cluster_id"`
+
+	// CommonName is the common name used to configure the root CA associated
+	// with the current PKI backend.
+	CommonName string `json:"common_name"`
+
+	// TTL configures the time to live for the requested certificate. This is a
+	// golang time string with the allowed units s, m and h.
+	TTL string `json:"ttl"`
+}
+
+// PKIController manages the setup of Vault's PKI backends and all other
+// required steps necessary to be done.
+type PKIController interface {
+	// PKI management.
+
+	// SetupPKIBackend sets up a Vault PKI backend according to the given
+	// configuration.
+	SetupPKIBackend(config PKIConfig) error
+
+	// Path management.
+
+	// CAPath returns the path under which a cluster's certificate authority can
+	// be generated. This is very specific to Vault. The path structure is the
+	// following. See also https://github.com/hashicorp/vault/blob/6f0f46deb622ba9c7b14b2ec0be24cab3916f3d8/website/source/docs/secrets/pki/index.html.md#pkirootgenerate.
+	//
+	//     pki-<clusterID>/root/generate/exported
+	//
+	CAPath(clusterID string) string
+
+	// MountPath returns the path under which a cluster's PKI backend is mounted.
+	// This is very specific to Vault. The path structure is the following.
+	//
+	//     pki-<clusterID>
+	//
+	MountPath(clusterID string) string
+
+	// RolePath returns the path under which a role is registered. This is very
+	// specific to Vault. The path structure is the following. See also https://github.com/hashicorp/vault/blob/6f0f46deb622ba9c7b14b2ec0be24cab3916f3d8/website/source/docs/secrets/pki/index.html.md#pkiroles.
+	//
+	//     pki-<clusterID>/roles/role-<clusterID>
+	//
+	RolePath(clusterID string) string
+}
