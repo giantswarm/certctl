ef9dbda4d12a398d741ef285b0cd90c3100a8cfd
Tim Schindler <xh3b4sd@gmail.com>
2017-08-07 16:35:00 +0200

    moved to microerror/micrologger

--- service/cert-signer/error.go
+++ service/cert-signer/error.go
@@ -1,37 +1,19 @@
 package certsigner
 
 import (
-	"fmt"
-
-	"github.com/juju/errgo"
+	"github.com/giantswarm/microerror"
 )
 
-var (
-	maskAny = errgo.MaskFunc(errgo.Any)
-)
-
-func maskAnyf(err error, f string, v ...interface{}) error {
-	if err == nil {
-		return nil
-	}
-
-	f = fmt.Sprintf("%s: %s", err.Error(), f)
-	newErr := errgo.WithCausef(nil, errgo.Cause(err), f, v...)
-	newErr.(*errgo.Err).SetLocation(1)
-
-	return newErr
-}
-
-var invalidConfigError = errgo.New("invalid config")
+var invalidConfigError = microerror.New("invalid config")
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
-	return errgo.Cause(err) == invalidConfigError
+	return microerror.Cause(err) == invalidConfigError
 }
 
-var keyPairNotFoundError = errgo.New("key pair not found")
+var keyPairNotFoundError = microerror.New("key pair not found")
 
 // IsKeyPairNotFound asserts keyPairNotFoundError.
 func IsKeyPairNotFound(err error) bool {
-	return errgo.Cause(err) == keyPairNotFoundError
+	return microerror.Cause(err) == keyPairNotFoundError
 }
